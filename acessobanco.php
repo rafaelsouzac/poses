73be616004299dc8c42ade652c4e73af8eda8987
Rafael Correa <rafaelcorrea2708@gmail.com>
2021-04-24 09:22:11 -0300

    Passo 1 do refactory constante no arquivo

--- acessobanco.php
+++ acessobanco.php
@@ -1,4 +1,30 @@
 <?php
+/* 
+
+Função que tem a responsabilidade de conectar ao SGDB Mysql ou MariaDB, Ver
+Documentação da infraestrutura do sistema para saber. Ainda faz a manipulação de 
+Comandos DML do SQL.
+
+Entrada: Recebe uma String contendo os DML do SQL.
+
+Saída: Retornar um objeto com o resultado do comando. Quando o comando é Select 
+retorna a tabela com os dados ou um campo vazio. Os demais comandos INSERT, UPDATE
+e DELETE, retorna o numero de linhas afetadas.
+
+Autor código: Rafael de Souza Corrêa
+Data da criação: fev. 2019
+
+Autor do Refactory: 
+Data da Revisão:
+O que foi executado:
+
+1) Gerou a documentação incial do código.
+1) Análise e se necessário alteração do padrão do código.
+2) Análise da lógica e o que pode ser alterado para diminuir o tamanho do código.
+3) Quais blocos de códigos podem tornar-se outras funções.
+4) Testar as entradas e saídas, garantindo o tipo de informação que será manipulada.
+
+*/
 
 function ConexaoBanco($strParamQuery)
 {
